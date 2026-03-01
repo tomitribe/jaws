4642e5de0c220076b3e95a30ef4c75f33ecdd8cd
David Blevins <dblevins@tomitribe.com>
2026-02-28 17:01:45 -0800

    ChatGPTs attempt at converting to a v2 library

--- src/main/java/org/tomitribe/jaws/s3/S3Client.java
+++ src/main/java/org/tomitribe/jaws/s3/S3Client.java
@@ -16,37 +16,37 @@
  */
 package org.tomitribe.jaws.s3;
 
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.SdkClientException;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.transfer.TransferManager;
-import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
 import org.tomitribe.jaws.NoSuchBucketException;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
 
 import java.util.Iterator;
 import java.util.Spliterator;
 import java.util.Spliterators;
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class S3Client {
-    private final AmazonS3 s3;
+    private final software.amazon.awssdk.services.s3.S3Client s3;
+    private final S3AsyncClient s3Async;
+    private final S3TransferManager transferManager;
 
-    private final TransferManager transferManager;
-
-    public S3Client(final AmazonS3 s3) {
+    public S3Client(final software.amazon.awssdk.services.s3.S3Client s3, final S3AsyncClient s3Async) {
         this.s3 = s3;
-        this.transferManager = TransferManagerBuilder.standard()
-                .withS3Client(s3)
-                .withExecutorFactory(S3Client::createDefaultExecutorService)
+        this.s3Async = s3Async;
+        this.transferManager = S3TransferManager.builder()
+                .s3Client(s3Async)
+                .executor(createDefaultExecutorService())
                 .build();
     }
 
-    private static ThreadPoolExecutor createDefaultExecutorService() {
+    private static Executor createDefaultExecutorService() {
         ThreadFactory threadFactory = new ThreadFactory() {
             private int threadCount = 1;
 
@@ -57,7 +57,7 @@ public class S3Client {
                 return thread;
             }
         };
-        return (ThreadPoolExecutor) Executors.newFixedThreadPool(10, threadFactory);
+        return Executors.newFixedThreadPool(10, threadFactory);
     }
 
     public static <T> Stream<T> asStream(final Iterator<T> iterator) {
@@ -66,18 +66,22 @@ public class S3Client {
                 false);
     }
 
-    public S3Bucket createBucket(final String s) throws SdkClientException, AmazonServiceException {
-        final Bucket bucket = s3.createBucket(s);
+    public S3Bucket createBucket(final String name) {
+        s3.createBucket(CreateBucketRequest.builder().bucket(name).build());
+
+        final Bucket bucket = s3.listBuckets().buckets().stream()
+                .filter(item -> name.equals(item.name()))
+                .findAny()
+                .orElseThrow(() -> new NoSuchBucketException(name));
+
         return new S3Bucket(this, bucket);
     }
 
     public S3Bucket getBucket(final String name) {
-        /*
-         * There doesn't seem to be a way to get a bucket
-         * directly by name.
-         */
-        final Bucket bucket = s3.listBuckets().stream()
-                .filter(item -> name.equals(item.getName()))
+        final ListBucketsResponse bucketsResponse = s3.listBuckets();
+
+        final Bucket bucket = bucketsResponse.buckets().stream()
+                .filter(item -> name.equals(item.name()))
                 .findAny()
                 .orElseThrow(() -> new NoSuchBucketException(name));
 
@@ -85,16 +89,19 @@ public class S3Client {
     }
 
     public Stream<S3Bucket> buckets() {
-        return s3.listBuckets().stream()
+        return s3.listBuckets().buckets().stream()
                 .map(bucket -> new S3Bucket(this, bucket));
-
     }
 
-    public AmazonS3 getS3() {
+    public software.amazon.awssdk.services.s3.S3Client getS3() {
         return s3;
     }
 
-    public TransferManager getTransferManager() {
+    public S3AsyncClient getS3Async() {
+        return s3Async;
+    }
+
+    public S3TransferManager getTransferManager() {
         return transferManager;
     }
 }
